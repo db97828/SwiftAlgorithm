7a9eaf0166864433f5ae311d8e1030ea134f761e
db97828 <64210324+db97828@users.noreply.github.com>
2021-08-12 17:02:32 +0900

    Update PM_K번째수.swift

--- PM_K번째수.swift
+++ PM_K번째수.swift
@@ -6,11 +6,11 @@ func solution_k번째수_1(_ array:[Int], _ commands:[[Int]]) -> [Int] {
     
     commands.forEach{
         var array = array
-        let i = $0[0]
-        let j = $0[1]
-        let k = $0[2]
-        array = array[i-1...j-1].sorted()
-        answer.append(array[k-1])
+        let i = $0[0]-1
+        let j = $0[1]-1
+        let k = $0[2]-1
+        array = array[i...j].sorted()
+        answer.append(array[k])
     }
     return answer
 }
