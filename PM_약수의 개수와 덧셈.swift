e2b64bb302e59bf1a69e83a5f01b7d20da583a63
db97828 <64210324+db97828@users.noreply.github.com>
2021-08-17 17:16:38 +0900

    Create PM_약수의 개수와 덧셈.swift

---
+++ PM_약수의 개수와 덧셈.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution약수의_개수와_덧셈_1(_ left:Int, _ right:Int) -> Int {
+    var answer = 0
+    for i in left...right {
+        var divisor = 0
+        for j in 1...i {
+            if i % j == 0 { divisor += 1 }
+        }
+        if divisor % 2 == 0 {answer += i}
+        else {answer -= i}
+    }
+    return answer
+}
+
+func solution약수의_개수와_덧셈_2(_ left:Int, _ right:Int) -> Int {
+    return (left...right).map{ i in (1...i).filter{$0 % i == 0}.count % 2 == 0 ? i : -i}.reduce(0, +)
+}
