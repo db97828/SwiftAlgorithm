128523ea37b098a6c468895669efc294a30cb59e
db97828 <64210324+db97828@users.noreply.github.com>
2021-09-06 10:03:32 +0900

    Update PM_괄호_변환.swift

--- PM_괄호_변환.swift
+++ PM_괄호_변환.swift
@@ -1,18 +1,17 @@
-func solution(_ p:String) -> String {
+import Foundation
+
+func solution_괄호_변환(_ p:String) -> String {
     var answer = ""
-    //1번
-    if p.isEmpty { return answer}
-    //2번
+    if p == answer { return answer }
+    
     let (u,v) = divide(p)
-
-    if isCorrected(u) { return u + solution(v) }
-    else{
-        answer += "("
-        answer += solution(v)
-        answer += ")"
-        answer += u.dropFirst().dropLast().map{ $0 == "(" ? ")" : "(" }
+    
+    if isCorrencted(u) { return u + solution_괄호_변환(v) }
+    else {
+        answer += "(\(solution_괄호_변환(v)))"
+        answer += u.dropFirst().dropLast().map{$0 == "(" ? ")" : "("}
     }
-
+    
     return answer
 }
 
@@ -20,27 +19,30 @@ func divide(_ p: String) -> (String, String) {
     let p = p.map{$0}
     var left = 0
     var right = 0
-
+    
     for i in 0..<p.count {
         if p[i] == "(" {left += 1}
         else {right += 1}
-        if left == right {return (String(p[...i]), String(p[Int(i+1)...])) }
-    }
-    return ("","")
-}
-
-func isCorrected(_ u: String) -> Bool {
-    let u = u.map{$0}
-    var stack = [String]()
-    if u.first == ")" {return false}
-
-    for i in u {
-        if i == "(" {stack.append(String(i))}
-        else{
-            if stack.isEmpty {return false}
-            let _ = stack.popLast()
+        if left == right {
+            return (String(p[...i]),String(p[Int(i+1)...]))
         }
     }
+    
+    return (String(p),"")
+}
 
+func isCorrencted(_ u: String) -> Bool {
+    let u = u.map{$0}
+    var stack = [String]()
+    
+    if u.first == ")" {return false}
+    
+    for i in u {
+        if i == "(" { stack.append(String(i))}
+        else {
+            if stack.isEmpty {return false}
+            stack.popLast()
+        }
+    }
     return true
 }
