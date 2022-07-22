bac921dfb5e6e525d1328c31b0e6e6431f800f40
Davy <64210324+db97828@users.noreply.github.com>
2022-07-22 16:23:39 +0900

    Update Leet191.swift

--- Leet191.swift
+++ Leet191.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class Solution_191 {
-    func hammingWeight(_ n: Int) -> Int {
+    func hammingWeight_1(_ n: Int) -> Int {
         var number = n
         var answer: Int = 0
         while number != 0 {
@@ -16,5 +16,9 @@ class Solution_191 {
             number = number >> 1
         }
         return answer
+    }
+    
+    func hammingWeight_2(_ n: Int) -> Int {
+        return String(n, radix: 2).filter { $0 == "1" }.count
     }
 }
